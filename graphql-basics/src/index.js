0c58ce3652245df86a035f8abd4cd101099a840e
José Silva <jcnascimento1994@gmail.com>
2019-12-24 10:45:11 -0300

    Operaçoes "delete"

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,7 +1,7 @@
 import { GraphQLServer } from 'graphql-yoga';
 import uuid from 'uuid/v4';
 
-const users = [
+let users = [
   {
     id: '1',
     name: 'John Doe',
@@ -21,7 +21,7 @@ const users = [
   }
 ];
 
-const posts = [
+let posts = [
   {
     id: '12',
     title: 'so theory',
@@ -45,7 +45,7 @@ const posts = [
   }
 ];
 
-const comments = [
+let comments = [
   {
     id: '11',
     author: '1',
@@ -61,13 +61,13 @@ const comments = [
   {
     id: '33',
     author: '2',
-    post: '15',
+    post: '14',
     body: 'Of course what?'
   },
   {
     id: '21',
     author: '3',
-    post: '15',
+    post: '14',
     body: 'Of course'
   }
 ];
@@ -83,8 +83,13 @@ const typeDefs = `
 
     type Mutation {
       createUser(data: CreateUserInput!): User!
+      deleteUser(id: ID!): User!
+
       createPost(data: CreatePostInput!): Post!
+      deletePost(id: ID!): Post!
+
       createComment(data: CreateCommentInput!): Comment!
+      deleteComment(id: ID!): Comment!
     }
 
     input CreateUserInput {
@@ -183,6 +188,28 @@ const resolvers = {
 
       return user;
     },
+    deleteUser(parent, args, ctx, info) {
+      const userFound = users.find(user => user.id === args.id);
+
+      if (!userFound) throw new Error('User not found!');
+
+      users = users.filter(user => user.id !== args.id);
+
+      posts = posts.filter(post => {
+        const match = post.author === args.id;
+
+        if (match) {
+          comments = comments.filter(comment => comment.post !== post.id);
+        }
+
+        return !match;
+      });
+
+      comments = comments.filter(comment => comment.author !== args.id);
+
+      return userFound;
+    },
+
     createPost(parent, args, ctx, info) {
       const userExists = users.some(user => user.id === args.data.author);
 
@@ -197,13 +224,31 @@ const resolvers = {
 
       return newPost;
     },
+    deletePost(parent, args, ctx, info) {
+      const deletedPost = posts.find(post => post.id === args.id);
+
+      if (!deletedPost) throw new Error('Post not found');
+
+      posts = posts.filter(post => {
+        const match = post.id === args.id;
+
+        if (match) {
+          comments = comments.filter(comment => comment.post !== post.id);
+        }
+
+        return !match;
+      });
+
+      return deletedPost;
+    },
+
     createComment(parent, args, ctx, info) {
       const userExists = users.some(user => user.id === args.data.author);
       const postExists = posts.some(post => post.id === args.data.post);
 
-      if (!userExists) throw new Error('Author does not exist!');
+      if (!userExists) throw new Error('Author not found!');
 
-      if (!postExists) throw new Error('Post does not exist!');
+      if (!postExists) throw new Error('Post not found!');
 
       const newComment = {
         id: uuid(),
@@ -211,6 +256,15 @@ const resolvers = {
       };
 
       return newComment;
+    },
+    deleteComment(parent, args, ctx, info) {
+      const deletedComment = comments.find(comment => comment.id === args.id);
+
+      if (!deletedComment) throw new Error('Comment not found');
+
+      comments = comments.filter(comment => comment.id !== args.id);
+
+      return deletedComment;
     }
   },
   Post: {
